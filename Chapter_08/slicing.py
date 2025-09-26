0dbf8d2bda04b18a1d10c8860ce4788b355bd1ec
rainer Telesko <rainer.telesko@bluewin.ch>
2025-09-26 18:46:40 +0200

    2025-09-26: Adding slicing.py

---
+++ Chapter_08/slicing.py
@@ -0,0 +1,18 @@
+# Slicing with Strings
+
+s = "Telesko"
+
+print(s[1:5])
+print(s[1:])
+print(s[:1])
+print(s[:])
+print(s[::])
+print(s[:-1])
+print(s[:-2])
+print(s[::-1])
+print(s[-4:])
+print(s[-4:-2])
+print(s[:-4])
+print(s[::len(s)-1])
+print(s[-4:0:-2])
+
