9ba2509685f04b23927fa8bc61a8c2c9f5f021a9
rtelesko <rainer.telesko@fhnw.ch>
2025-09-30 07:55:14 +0200

    2025-09-30: Create comments in slicing.py

--- Chapter_08/slicing.py
+++ Chapter_08/slicing.py
@@ -2,17 +2,17 @@
 
 s = "Telesko"
 
-print(s[1:5])
-print(s[1:])
-print(s[:1])
-print(s[:])
-print(s[::])
-print(s[:-1])
-print(s[:-2])
-print(s[::-1])
-print(s[-4:])
-print(s[-4:-2])
-print(s[:-4])
-print(s[::len(s)-1])
-print(s[-4:0:-2])
+print(s[1:5]) # eles
+print(s[1:]) # elesko
+print(s[:1]) # T
+print(s[:]) # Telesko
+print(s[::]) # Telesko
+print(s[:-1]) # Telesk
+print(s[:-2]) # Teles
+print(s[::-1]) # okseleT
+print(s[-4:]) # esko
+print(s[-4:-2]) # es
+print(s[:-4]) # Tel
+print(s[::len(s)-1]) # To
+print(s[-4:0:-2]) # ee
 
